4f5d69ae079f64c300a82f68d1453e64999c0dd8
chinmaya-sajeevan <chinmayapjm@gmail.com>
2025-11-26 14:51:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as s
+s.title("Welcome")
+s.subheader("Title Experiments")
+s.text("Cool Features")
+s.markdown("**Hello** *World*")
+s.markdown("[Google](https://google.com)")
+s.markdown("---")
+s.image("img2.jpeg",caption="Dog Image")
+s.video("video1.mp4")
+
